af49703c14f97a0fadaff2f60869ce9c01a95d51
Gira306 <dphuocm.mai@gmail.com>
2025-04-21 22:27:48 +0700

    Update genre.js

--- gaumeoteam/src/genre.js
+++ gaumeoteam/src/genre.js
@@ -1,16 +1,8 @@
 function execute() {
-    load('config.js');
-    let response = fetch(BASE_URL + "/manga-genre");
-    if (response.ok) {
-        let doc = response.html();
-        doc.select(".number-story").remove();
-        let genre = [];
-        doc.select(".page-genres a").forEach(e => genre.push({
-            title: e.text(),
-            input: e.attr("href"),
-            script: "gen.js"
-        }));
-        return Response.success(genre);
-    }
-    return null;
+    return Response.success([
+        {title: "Mới cập nhật", input: BASE_URL + "", script: "cat.js"},
+        {title: "Novel", input: BASE_URL + "/manga-genre/tieu-thuyet-bl/", script: "gen.js"},
+        {title: "Đang tiến hành", input: BASE_URL + "/manga-genre/dang-tien-hanh/", script: "gen.js"},
+        {title: "Đã hoàn thành", input: BASE_URL + "/manga-genre/hoan-thanh/", script: "gen.js"}
+    ]);	
 }
