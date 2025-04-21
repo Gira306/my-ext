c36ae6392ab21b352f1ea9964ef165dc66da4aef
Gira306 <dphuocm.mai@gmail.com>
2025-04-21 22:19:05 +0700

    Update home.js

--- gaumeoteam/src/home.js
+++ gaumeoteam/src/home.js
@@ -1,8 +1,8 @@
 function execute() {
     return Response.success([
-        {title: "Mới cập nhật", input: "", script: "cat.js"},
-        {title: "Novel", input: "/genre/novel", script: "gen.js"},
-        {title: "Đang tiến hành", input: "/genre/ongoing", script: "gen.js"},
-        {title: "Đã hoàn thành", input: "/genre/complete", script: "gen.js"}
+        {title: "Mới cập nhật", input: BASE_URL + "", script: "cat.js"},
+        {title: "Novel", input: BASE_URL + "/manga-genre/tieu-thuyet-bl/", script: "gen.js"},
+        {title: "Đang tiến hành", input: BASE_URL + "/manga-genre/dang-tien-hanh/", script: "gen.js"},
+        {title: "Đã hoàn thành", input: BASE_URL + "/manga-genre/hoan-thanh/", script: "gen.js"}
     ]);	
 }
